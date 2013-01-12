76e893fefba846ca60068427310571ec3440c27d
Jesse Armand <jesse@jessearmand.com>
2013-01-12 23:40:33 +0800

    Initial Commit

---
+++ Exposer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Exposer
+//
+//  Created by Jesse Armand on 12/1/13.
+//  Copyright (c) 2013 Jesse Armand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Exposer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Exposer
+//
+//  Created by Jesse Armand on 12/1/13.
+//  Copyright (c) 2013 Jesse Armand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
