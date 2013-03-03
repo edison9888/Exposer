b8918c62b43507709f69c341e5a2730a56d21d9b
Jesse Armand <jesse@jessearmand.com>
2013-03-03 16:44:30 +0800

    A change in project structure.
    This is the demo app project. Will prepare for the framework project.

---
+++ Exposer/Exposer.h
@@ -0,0 +1,11 @@
+//
+//  Exposer.h
+//  Exposer
+//
+//  Created by Jesse Armand on 3/3/13.
+//  Copyright (c) 2013 Jesse Armand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
